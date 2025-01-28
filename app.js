1f51738d339c82e90c67211fd44e8e834d0e87eb
Preet Ramsha <preetramsha123@gmail.com>
2025-01-29 01:05:18 +0530

    minor improvements

--- app.js
+++ app.js
@@ -3,14 +3,31 @@ import { Hono } from "hono";
 import { generateImageWithText } from "./src/lib";
 const app = new Hono();
 
+app.get("/", (c) => {
+  return c.text(`
+    Welcome! There are 2 routes you can access:
+    
+    1. /readable-stream/:amount - This route generates an image with text and returns a readable stream.
+    2. /dataurl/:amount - This route generates an image with text and returns the image as a data URL.
+
+    Example usage:
+    - /readable-stream/100
+    - /dataurl/100
+  `);
+});
+
 app.get("readable-stream/:amount", (c) => {
-  const amount = c.req.param("amount");
+  let amount = c.req.param("amount");
+  // Remove dollar symbol and commas
+  amount = amount.replace(/[$,]/g, "");
   const resp = generateImageWithText(amount, "./pricedow.ttf");
   return c.text(resp.readableStream);
 });
 
 app.get("dataurl/:amount", (c) => {
-  const amount = c.req.param("amount");
+  let amount = c.req.param("amount");
+  // Remove dollar symbol and commas
+  amount = amount.replace(/[$,]/g, "");
   const resp = generateImageWithText(amount, "./pricedow.ttf");
   return c.text(resp.dataURL);
 });
